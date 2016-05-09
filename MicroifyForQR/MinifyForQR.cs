bee1814f38d20d03763e4b290b0c61a920e361c2
niiiiklas <niklas.emil.andersson@gmail.com>
2016-05-09 23:02:52 +0200

    initial commit with test console app

---
+++ MicroifyForQR/MinifyForQR.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niklas.MicroifyForQR
+{
+    public class MinifyForQR
+    {
+
+        public static string CreateString<T>(T obj)
+        {
+            string ret = "";
+            foreach(PropertyInfo prop in obj.GetType().GetProperties())
+            {
+
+                var minifyAs = prop.GetCustomAttribute<Niklas.MicroifyForQR.MinifyAs>();
+                if(minifyAs != null)
+                {
+                    ret += minifyAs.MinifiedProperty;
+                }
+                else
+                {
+                    ret += prop.Name.Substring(0,3);
+                }
+
+                ret += ":";
+
+                ret += prop.GetValue(obj);
+
+                ret += "-";
+            }
+            return ret;
+        }
+    }
+
+
+    [System.AttributeUsage(AttributeTargets.Property)]
+    public class MinifyAs:System.Attribute
+    {
+        public string MinifiedProperty { get; set; }
+        public MinifyAs(string minifiedPropertyName = "")
+        {
+            MinifiedProperty = minifiedPropertyName;
+        }
+    }
+
+    [System.AttributeUsage(AttributeTargets.Property)]
+    public class IgnoreProperty:System.Attribute
+    {
+
+    }
+}
---
+++ MiniConsoleTest/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using Niklas.MicroifyForQR;
+
+namespace MiniConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        public Program()
+        {
+            Console.WriteLine("Hello!");
+
+            var td = new TestData() { TheNameOfSubject = "Niklas Andersson", SomethingElse = "12" };
+
+            foreach(PropertyInfo pi in td.GetType().GetProperties())
+            {
+                string str = "";
+                str += pi.GetValue(td) + " : ";
+                str += pi.Name;
+                var hasIt = pi.GetCustomAttributes<Niklas.MicroifyForQR.MinifyAs>();
+                Console.WriteLine(str + " " + hasIt);
+            }
+
+            Console.WriteLine("----------------------");
+            Console.WriteLine("" + Niklas.MicroifyForQR.MinifyForQR.CreateString<TestData>(td));
+
+            Console.ReadLine();
+
+        }
+    }
+
+
+
+
+    public class TestData
+    {
+        [MinifyAs("tsd")]
+        public string TheNameOfSubject { get; set; }
+
+        public string SomethingElse { get; set; }
+    }
+}
